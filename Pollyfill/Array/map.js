e97285fa9bd48383aa70ac91998a06bd7bd32a30
vengadeshraj <vengadeshraj405@gmail.com>
2025-07-17 00:30:36 +0530

    Array map pollyfill description added

--- Pollyfill/Array/map.js
+++ Pollyfill/Array/map.js
@@ -1,10 +1,17 @@
+// One of the Non mutating array method
+// It will take three arguments in the callback function
+// Example callback(value, index, array)
+// value ---> element of current loop
+// index ---> index of the each loop
+// array ---> whole array itself
+
 Array.prototype.customMap = function(callbackFn){
     // Create result array to store response
     const result = [];
     // Iterating array
         for(let i =0;i<this.length;i++){
             // Pushing response of callback function into result
-            result.push(callbackFn(this[i],i));
+            result.push(callbackFn(this[i],i,this));
         }
     // return response
     return result;
