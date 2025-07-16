4093e27e593dacde6e0920fe57dbeb64fa2d23ea
vengadeshraj <vengadeshraj405@gmail.com>
2025-07-17 00:08:28 +0530

    Array join comments added

--- Pollyfill/Array/join.js
+++ Pollyfill/Array/join.js
@@ -1,15 +1,18 @@
 Array.prototype.customJoin = function(joinString){
+     // Create result string to store response
     let result = "";
-
+    // Iterating array
     for(let i =0;i<this.length;i++){
+        // Check if i is 0 then directly append element to result
         if(i==0){ 
             result = result + this[i]
         }
+        // Append each element with joining string
         else {
             result = result + joinString + this[i]
         }
     }
-
+    //Return response
     return result;
 };
 
