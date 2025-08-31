648b7419d7070400d3c16984d711f4a43593066b
vengadeshraj <vengadeshraj405@gmail.com>
2025-08-31 22:19:21 +0530

    pollyfill type modified

---
+++ Pollyfill/Array/immutable/join.js
@@ -0,0 +1,33 @@
+// join ---> Joins all elements of an array into a string
+// It will take one arugument that is separater, By default it will be "," 
+// and also we can change the separator as per our wish
+
+Array.prototype.customJoin = function(separator=","){ // Setting default separator value as ","
+    // Convert separator into string to avoid unexpected type mismatch 
+    separator = String(separator);
+     // Create result string to store response
+    let result = "";
+    // Iterating array
+    for(let i =0;i<this.length;i++){
+        // Check if i is 0 then directly append element to result
+        if(i==0){ 
+            result = result + this[i]
+        }
+        // Append each element with separator string
+        else {
+            result = result + separator + this[i]
+        }
+    }
+    //Return response
+    return result;
+};
+
+
+const test = [1,2,3,4,5];
+
+const cusJoinResponse = test.customJoin("-");
+
+const joinResponse =  test.join("-");
+
+console.log(cusJoinResponse);
+console.log(joinResponse);
---
+++ Pollyfill/Array/immutable/map.js
@@ -0,0 +1,29 @@
+// Map ---> Creates a new array with the results of calling a function on every element in the array.
+// It will take three arguments in the callback function
+// Example callback(value, index, array)
+// value ---> element of current loop
+// index ---> index of the each loop
+// array ---> whole array itself
+
+Array.prototype.customMap = function(callbackFn){
+    // Create result array to store response
+    const result = [];
+    // Iterating array
+        for(let i =0;i<this.length;i++){
+            // Pushing response of callback function into result
+            result.push(callbackFn(this[i],i,this));
+        }
+    // return response
+    return result;
+};
+
+
+const test = [1,2,3,4];
+
+const cusMapResult = test.customMap((ele,i)=> ele*i);
+const mapResult = test.map((ele,i)=> ele*i);
+
+console.log(cusMapResult);
+
+console.log(mapResult);
+
