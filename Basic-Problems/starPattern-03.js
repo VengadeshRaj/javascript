3681a1dc5677853ccfc5050a227d575d739929eb
Vengadesh Raj <vengadeshraj405@gmail.com>
2026-06-02 23:43:18 +0530

    basic problem changes added

--- Basic-Problems/starPattern-03.js
+++ Basic-Problems/starPattern-03.js
@@ -1,1 +1,11 @@
-console.log("need to start dev...");
+
+
+const n = 5;
+
+for(let i =0;i<n;i++){
+    let rows = "";
+    for(let j=0;j<n;j++){
+        rows= rows + "*"
+    }
+    console.log(rows)
+}
