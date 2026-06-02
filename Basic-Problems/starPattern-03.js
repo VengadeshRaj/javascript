8f48006b1a67ae6a29cbaa8f55a6a92665142c77
Vengadesh Raj <vengadeshraj405@gmail.com>
2026-06-02 23:50:46 +0530

    basic problem changes inprogress

--- Basic-Problems/starPattern-03.js
+++ Basic-Problems/starPattern-03.js
@@ -2,10 +2,14 @@
 
 const n = 5;
 
-for(let i =0;i<n;i++){
+for(let i =n;i>0;i--){
     let rows = "";
-    for(let j=0;j<n;j++){
-        rows= rows + "*"
+    for(let j=0;j<n;j++){ // 5 1
+        if(i == j || i<j) {
+            console.log(i,j)
+            rows = rows+"*"
+        }
+        else rows = rows = "-"
     }
     console.log(rows)
 }
