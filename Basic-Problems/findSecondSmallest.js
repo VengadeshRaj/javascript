a08974e692371b01b9a0f3ac8309fca875cb7cd3
Vengadesh Raj <vengadeshraj405@gmail.com>
2026-06-07 16:56:02 +0530

    second smallest problem added

---
+++ Basic-Problems/findSecondSmallest.js
@@ -0,0 +1,21 @@
+function findSecondSmallest(numbers) {
+  let smallest = Infinity;
+  let secondSmallest = Infinity;
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (smallest > numbers[i]) {
+      secondSmallest = smallest;
+      smallest = numbers[i];
+    } else if (secondSmallest > numbers[i]) {
+      secondSmallest = numbers[i];
+    }
+  }
+
+  return secondSmallest;
+}
+
+const input = [45,67,6,12,33,89,4,12,90,32];
+
+const result = findSecondSmallest(input);
+
+console.log(result);
